9640594c4a28f3710439151bf97ea22e67873039
chaelin1211 <cofls749@naver.com>
2019-07-18 17:32:57 +0900

    commit

--- app.js
+++ app.js
@@ -37,16 +37,39 @@ router.get('/c_13', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/chaelin/c_13_form.html'))
     //__dirname : It will resolve to your project folder.
 });
+router.get('/c_14', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/chaelin/c_14_components.html'))
+    //__dirname : It will resolve to your project folder.
+});
+router.get('/c_15', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/chaelin/c_15_card.html'))
+    //__dirname : It will resolve to your project folder.
+});
 router.get('/20190718form', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/seonghoon/20190718/formgroup.html'))
     //__dirname : It will resolve to your project folder.
 });
 router.get('/detail', function (req, res) {
     console.log(req.query.email);
-    res.send('Request parameters : '+req.query.email+', '+req.query.status)
-    });
+    res.send('Request parameters : ' + req.query.email + ', ' + req.query.status)
+});
 
-router.get('/20190718submit',function(req,res){console.log(req.query); res.send('Request parameters : '+ req.query.email+','+ req.query.status)});
+router.get('/20190718submit', function (req, res) { console.log(req.query); res.send('Request parameters : ' + req.query.email + ',' + req.query.status) });
 
+router.get('/pro2', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/chaelin/project/0718_project.html'))
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/proj_ans_01', function (req, res) {
+    res.send('Request parameters : email: ' + req.query.email + ' / password: ' + req.query.psw + ' / remember? ' + req.query.remember)
+});
+
+router.get('/proj_ans_02', function (req, res) {
+    res.send('Request parameters : email: ' + req.query.email + ' / password: ' + req.query.psw + ' / text: ' + req.query.text)
+});
+router.get('/proj_ans_03', function (req, res) {
+    res.send('Request parameters : select1: ' + req.query.sel1 + ' / select2: ' + req.query.sel2 )
+});
 app.use('/', router);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
